d06537fa2cb1650a49ee019c5c782721dda31e62
Anton <100570785@alumnos.uc3m.es>
2026-03-03 12:30:35 +0100

    Comentarios

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -7,7 +7,7 @@ import com.google.genai.types.Part;
 
 public class Client {
     private final com.google.genai.Client client;
-    private final String model = "gemini-2.5-flash";
+    private final String model = "gemini-2.5-flash"; // Hay que decidir que modelo usar
 
     public Client(String API_KEY) {
         client = com.google.genai.Client.builder().apiKey(API_KEY).build();
@@ -19,6 +19,7 @@ public class Client {
     }
 
     public String queryConConfig(String location) {
+        // Este texto habra que cambiarlo cuando probemos con Gemini
         String query = "I am going to give you a location name. Please answer ONLY with the URL of the English version of Wikipedia (starting with https://en.wikipedia.org/) that most likely identifies that location. ";
         GenerateContentConfig config =
                 GenerateContentConfig.builder().systemInstruction(
